abebaf7f72612dae5894e430e9be97ed9105f427
nandinineogen-glitch <nandinineogen@gmail.com>
2026-08-05 07:54:46 +0530

    Delete src directory

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import colors from './colors';
-import assets from './assets';
-import components from './components';
-const utils = {
-  colors: colors,
-  assets: assets,
-  components:  components,
-};
-export default utils;
